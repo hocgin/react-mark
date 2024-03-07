cd19fa095278c4658c2daba09d3d9b4b5b4daf4a
hocgin <hocgin@gmail.com>
2024-03-07 23:23:50 +0800

    dev

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,9 +1,22 @@
-import { defineConfig } from 'dumi';
+import {defineConfig} from 'dumi';
 
+export const useLogger = () => {
+  let result = [];
+  let offLogger = process.env.USE_LOG !== 'true';
+  console.debug(`[${offLogger ? '禁用' : '启用'}]日志打印`);
+  if (offLogger) {
+    result.push([
+      'transform-remove-console',
+      {exclude: ['error', 'warn', 'info']},
+    ]);
+  }
+  return result;
+};
 export default defineConfig({
   outputPath: 'docs-dist',
   themeConfig: {
     name: '@hocgin/marks',
   },
+  extraBabelPlugins: [...useLogger()],
   ssr: process.env.NODE_ENV === 'development' ? false : {},
 });
