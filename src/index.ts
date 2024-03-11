43557148dbd896f8224467af4e07cc3b36b119f9
hocgin <hocgin@gmail.com>
2024-03-11 22:39:18 +0800

    dev

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export {useMark, useMarkNote} from './marks2';
 export {Panel, ColorSelect} from './panel';
+export {useMarkJS} from './util/useMarkJS';
 export {MarkCard, MarkNoteCard} from './MarkCard';
